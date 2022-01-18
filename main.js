26d2b363bf9c826abbb34eab3842c6f048ae9161
iMeece <MeeceCoding@gmail.com>
2022-01-18 08:37:59 +0200

    Added about section filler, created sliding animation for header titles, added button styling, new font, some minor tweaks

---
+++ main.js
@@ -0,0 +1,1 @@
+(()=>{let e=!0;document.addEventListener("scroll",(function(t){const n=document.getElementById("about");window.scrollY+window.innerHeight>n.offsetTop+n.offsetHeight/2&&e&&(e=!1)}))})();
